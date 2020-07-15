c3b14f453f501e9149ab3303fc48fdf6bd3f81f1
giovanni-bort <63361927+giovanni-bort@users.noreply.github.com>
2020-07-15 09:49:05 +0200

    Add files via upload

---
+++ load_12ECG_model.m
@@ -0,0 +1,11 @@
+function model = load_12ECG_model(model_directory)
+	
+model=0;	
+% out_file='finalized_model.mat';
+% 	filename=fullfile(model_directory,out_file);
+%         A=load(filename);
+%         model=A.model;
+
+end
+
+
